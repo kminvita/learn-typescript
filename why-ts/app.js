061111030f38b753b4b1d90e51406908caffe966
kminvita <vitamink1206@gmail.com>
2023-09-02 23:50:02 +0900

    [S1U3] 왜 타입스크립트를 쓰면 좋은가?

--- why-ts/app.js
+++ why-ts/app.js
@@ -1,5 +1,5 @@
 // api url
-var url = 'https://jsonplaceholder.typicode.com/users/1';
+var url = 'https://jsonplaceholder.typicode.com/users/';
 
 // dom
 var username = document.querySelector('#username');
@@ -13,9 +13,15 @@ function startApp() {
   axios
     .get(url)
     .then(function (response) {
-      console.log(response);
+      // console.log(response);
       user = response.data;
       // TODO: 이름, 이메일, 주소 표시하기
+      console.log(user);
+      username.innerText = user[0].name;
+      email.innerText = user[0].email;
+      address.innerText = user[0].address.street; 
+      // 여기서 address라고 안하고 addres 와 같이 오탈자로 표기할 경우 코드상에서 오류는 발생하진 않지만,
+      // live server로 확인했을때 undefined로 나타나는걸 확인할 수 있다.
     })
     .catch(function (error) {
       console.log(error);
